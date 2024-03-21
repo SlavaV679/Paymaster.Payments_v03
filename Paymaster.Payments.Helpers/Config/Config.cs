0d646e0c5471e47a44e8bf1f296357759047bc19
Slava <v.vorobjev@hq.runpay.com>
2024-03-21 16:41:09 +0300

    add static class Config

---
+++ Paymaster.Payments.Helpers/Config/Config.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Paymaster.Payments.Helpers.Config
+{
+    public static class Config
+    {
+        public static string PaymentsConnectionString { get; set; }
+
+        public static void LoadAppsettings(ConfigurationManager configuration)
+        {
+            PaymentsConnectionString = configuration.GetConnectionString("PaymentsConnectionString");
+        }
+    }
+}
